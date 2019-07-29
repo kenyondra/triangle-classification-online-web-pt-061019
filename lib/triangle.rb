dd12d7c7b68c3f0c8cdfd5d284eb523842e39d42
Kenyondra J Conley <kenyondra_conley@yahoo.com>
2019-07-29 13:50:41 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -11,6 +11,6 @@ class Triangle
     sum_one_three = @triangle_sides[0] + @triangle_sides[2]
     sum_two_three = @triangle_sides[1] + @triangle_sides[2]
     
-    if (@triangle_sides.none? {|side| side <= 0}) && (sum_one_two > @triangle_sides[2] && sum_one_three)
+    if (@triangle_sides.none? {|side| side <= 0}) && (sum_one_two > @triangle_sides[2] && sum_one_three > @triangle_sides[1] && sum)
   # write code here
 end
